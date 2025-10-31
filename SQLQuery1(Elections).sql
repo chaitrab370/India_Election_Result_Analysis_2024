7089c9f44c00b8f2a2c2831d3ddf7d3625670509
Chaitra B <chaitra.jmb0544@gmail.com>
2025-10-31 09:04:03 +0530

    Add files via upload

---
+++ SQLQuery1(Elections).sql
@@ -0,0 +1,21 @@
+SELECT DB_NAME() AS CurrentDatabase;
+
+SELECT name FROM sys.databases;
+
+USE "India Election Results";
+GO
+
+SELECT DB_NAME() AS CurrentDatabase;
+
+SELECT * FROM constituencywise_details;
+
+SELECT * FROM constituencywise_results;
+
+SELECT * FROM partywise_results;
+
+SELECT * FROM statewise_results;
+
+SELECT * FROM states;
+
+
+
